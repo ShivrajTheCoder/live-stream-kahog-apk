d882eb1001e1237b1dcd5d7cc2466902e92ec9bf
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-09 20:07:39 +0530

    intial-commit

---
+++ src/KeyCenter.js
@@ -0,0 +1,9 @@
+const KeyCenter = {
+    appID: 2064458396,
+    appSign: '2b7e7257756571b2bac08637807500bdbdd5d3df879692751fa92a0099756442',
+    // apiUrl:"http://192.168.29.80:8082"
+    // apiUrl:"https://kahojibackend.onrender.com"
+}
+
+
+export default KeyCenter
