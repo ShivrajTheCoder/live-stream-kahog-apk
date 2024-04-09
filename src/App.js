17cb4a8e59c9832fe238163707398f402042a24f
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-09 20:53:59 +0530

    theme-added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { Component } from 'react';
 import AppNavigation from './AppNavigation';
-import {
-  ZegoUIKitPrebuiltLiveStreamingFloatingMinimizedView,
-} from '@zegocloud/zego-uikit-prebuilt-live-streaming-rn';
+import {ZegoUIKitPrebuiltLiveStreamingFloatingMinimizedView,} from '@zegocloud/zego-uikit-prebuilt-live-streaming-rn';
+import { ThemeProvider } from './contexts/ThemeProvider';
 
 export default function App() {
-  return (<NavigationContainer >
-    <AppNavigation />
-    <ZegoUIKitPrebuiltLiveStreamingFloatingMinimizedView />
-  </NavigationContainer>);
+  return (
+    <ThemeProvider>
+      <NavigationContainer >
+        <AppNavigation />
+        <ZegoUIKitPrebuiltLiveStreamingFloatingMinimizedView />
+      </NavigationContainer>
+    </ThemeProvider>);
 }
